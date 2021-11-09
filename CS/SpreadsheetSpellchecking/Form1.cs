3633d0f8b295dc97ca19bef966f949876df522b0
Natalya Senichkina <29920736+NatalyaSenichkina@users.noreply.github.com>
2021-11-09 16:00:57 +0300

    Add example files

---
+++ CS/SpreadsheetSpellchecking/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraSpellChecker;
+using DevExpress.XtraSpellChecker.Native;
+using DevExpress.XtraSpreadsheet.Internal;
+
+namespace SpreadsheetSpellchecking {
+    public partial class Form1 : RibbonForm {
+        SpellChecker spellChecker = new SpellChecker();
+        PopupMenu popupMenu = new PopupMenu();
+
+        public Form1() {
+            InitializeComponent();
+            SpellCheckTextControllersManager.Default.RegisterClass(typeof(TextBoxWithTransparency), typeof(SimpleTextBoxTextController));
+            SpellCheckTextBoxBaseFinderManager.Default.RegisterClass(typeof(TextBoxWithTransparency), typeof(TextBoxFinder));
+
+            spellChecker.SpellCheckMode = SpellCheckMode.AsYouType;
+
+            popupMenu.Manager = BarManager;
+            BarManager.QueryShowPopupMenu += OnBarManager_QueryShowPopupMenu;
+
+            spreadsheetControl.CellEditorOpened += OnSpreadsheetControl_CellEditorOpened;
+        }
+
+        BarManager BarManager => ribbonControl1.Manager;
+
+        void OnSpreadsheetControl_CellEditorOpened(object sender, DevExpress.XtraSpreadsheet.CellEditorOpenedEventArgs e) {
+            if (!e.IsCustom) {
+                BarManager.SetPopupContextMenu(e.Editor, popupMenu);
+                spellChecker.Check(e.Editor);
+            }
+        }
+
+        void OnBarManager_QueryShowPopupMenu(object sender, QueryShowPopupMenuEventArgs e) {
+            Point position = e.Control.PointToClient(e.Position);
+            DevExpress.XtraSpellChecker.Rules.SpellCheckErrorBase error = spellChecker.CalcError(position);
+            List<SpellCheckerCommand> commands = spellChecker.GetCommandsByError(error);
+            if (commands == null) {
+                e.Cancel = true;
+                return;
+            }
+
+            BarItemLinkCollection itemLinks = popupMenu.ItemLinks;
+            popupMenu.BeginUpdate();
+            try {
+                itemLinks.Clear();
+                foreach (SpellCheckerCommand command in commands) {
+                    BarButtonItem item = new BarButtonItem(BarManager, command.Caption);
+                    item.Enabled = command.Enabled;
+                    item.Tag = command;
+                    item.ItemClick += OnPopupMenu_ItemClick;
+                    itemLinks.Add(item);
+                }
+                BarButtonItem itemShowSpellingForm = new BarButtonItem(BarManager, "Show Spelling Form");
+                itemShowSpellingForm.ItemClick += OnPopupMenuShowSpellingForm_ItemClick;
+                itemLinks.Add(itemShowSpellingForm);
+            } finally {
+                popupMenu.EndUpdate();
+            }
+        }
+        void OnPopupMenu_ItemClick(object sender, ItemClickEventArgs e) {
+            var checkerCommand = e.Item.Tag as SpellCheckerCommand;
+            checkerCommand?.DoCommand();
+        }
+        void OnPopupMenuShowSpellingForm_ItemClick(object sender, ItemClickEventArgs e) {
+            spellChecker.Check(this.ActiveControl);
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            var activeSheet = spreadsheetControl.ActiveWorksheet;
+            activeSheet.Columns[1].WidthInPixels = 150;
+            spreadsheetControl.SelectedCell = activeSheet["B2"];
+            spreadsheetControl.SelectedCell.Value = "Missspelled wods";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Spreadsheet for WinForms – Spell Check Text in a Cell Editor
+
+This example demonstrates how to use the [DevExpress Spell Checker](https://docs.devexpress.com/WindowsForms/2635/controls-and-libraries/spell-checker) component to check spelling in spreadsheet cells. When a user types text in a cell, the spell checker indicates misspelled words with a red wavy line. The user can right-click an underlined word and select the correct spelling from the list of suggestions or invoke the [Spelling dialog](https://docs.devexpress.com/WindowsForms/8595/controls-and-libraries/spell-checker/visual-elements/spelling-dialog).
+
+![Spreadsheet - Spell Checking](./images/spreadsheet-spell-checking.png)
+
+This example uses the default English dictionary to check cell text. Refer to the following help topic for information on how to add dictionaries for other languages to the Spell Checker: [Spell Checker Dictionaries](https://docs.devexpress.com/WindowsForms/8581/controls-and-libraries/spell-checker/dictionaries).
+
+## Implementation Details
+
+Follow the steps below to integrate a spell checker into the Spreadsheet application:
+
+1. Register the control used as the default cell editor (the [System.Windows.Forms.TextBox](https://docs.microsoft.com/en-US/dotnet/api/system.windows.forms.textbox) descendant) to enable spell check in spreadsheet cells. See this topic for details: [Check Spelling in Different Controls](https://docs.devexpress.com/WindowsForms/2995/controls-and-libraries/spell-checker/checking-spelling-in-different-controls).
+
+    ```
+    SpellCheckTextControllersManager.Default.RegisterClass(typeof(TextBoxWithTransparency),
+        typeof(SimpleTextBoxTextController));
+    SpellCheckTextBoxBaseFinderManager.Default.RegisterClass(typeof(TextBoxWithTransparency),
+        typeof(TextBoxFinder));
+    ```
+
+2. Handle the [SpreadsheetControl.CellEditorOpened](https://docs.devexpress.com/WindowsForms/DevExpress.XtraSpreadsheet.SpreadsheetControl.CellEditorOpened) event to access the active cell editor. Use the [SpellChecker.Check](https://docs.devexpress.com/WindowsForms/DevExpress.XtraSpellChecker.SpellChecker.Check(System.Windows.Forms.Control)) method to check text in the editor.
+
+3. Create and display a **Spell Check** context menu for the cell editor, as described in this help topic: [Add a Spell Check Menu to the Standard Text Controls](https://docs.devexpress.com/WindowsForms/114007/controls-and-libraries/spell-checker/examples/how-to-add-spell-check-menu-to-the-standard-text-controls).
+
+<!-- default file list -->
+## Files to Look At
+
+- [Form1.cs](./CS/SpreadsheetSpellchecking/Form1.cs) ([Form1.vb](./VB/SpreadsheetSpellchecking/Form1.vb))
+<!-- default file list end -->
+
+## More Examples
+
+- [Spreadsheet for WPF – Spell Check Text in a Cell Editor](https://github.com/DevExpress-Examples/wpf-spreadsheet-spell-check-cell-text)
